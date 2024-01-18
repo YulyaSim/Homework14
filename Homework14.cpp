261a4cd64e115ec70591ef3f0635401d63dd7790
YulyaSim <152449597+YulyaSim@users.noreply.github.com>
2024-01-18 13:13:10 +1100

    Initial commit

---
+++ Homework14.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Enter any string:";
+    std::string a;
+    std::getline(std::cin, a);
+    std::cout << "\nString:" << a << "\nString lenght:" << a.size() << "\nFirst simbol:" << a[0] << "\nLast simbol:" << a[a.size() - 1];
+
+}
